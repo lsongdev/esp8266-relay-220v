382d32171a7b8457a501bc6f6d4dc889ee9f6005
Lsong <hi@lsong.org>
2023-11-08 14:35:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ESP8266 220V Relay
+
+<a href="https://www.taobao.com/list/item/662773208714.htm" >
+  <img alt="esp8266 12f二次开发DIY改装模块继电器wifi220v远程控制开关mqtt" src="https://gw.alicdn.com/imgextra/i1/376349686/O1CN01aABKQU2LQF1Ls3W2Z_!!376349686.jpg_468x468.jpg_.webp" />
+</a>
+
+<a href="https://item.taobao.com/item.htm?id=644551173284" >
+  <img src="https://img.alicdn.com/imgextra/i2/376349686/O1CN01NXKpKz2LQEzMGyU0E_!!376349686.jpg_.webp"/>
+</a>
+
+<https://lsong.org/notes/espx#esp8266-220v-relay>
---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid = "wifi@lsong.one";
+const char *password = "song940@163.com";
+const char *mqttServer = "mqtt.lsong.one";
+const char *clientID = "esp8266-relay-220v";
+const char *relayTopic = "esp8266-relay-220v";
+
+const int RY1 = 4;
+const int BTN1 = 12;
+const int LED1 = LED_BUILTIN;
+
+WiFiClient wlan;
+PubSubClient mqtt(wlan);
+
+void reconnect()
+{
+  while (!mqtt.connected())
+  {
+    Serial.println("Attempting to connect to the MQTT server...");
+    if (mqtt.connect(clientID))
+    {
+      Serial.println("MQTT connected");
+      mqtt.publish(relayTopic, "online");
+      mqtt.subscribe(relayTopic);
+    }
+    else
+    {
+      Serial.println("MQTT connect failed, retrying...");
+      delay(2000);
+    }
+  }
+}
+
+void onMessage(char *topic, byte *payload, unsigned int length)
+{
+  // Handle received messages
+  String payloadStr = "";
+  for (unsigned int i = 0; i < length; i++)
+  {
+    payloadStr += (char)payload[i];
+  }
+  Serial.println("Received message: [" + String(topic) + "] " + payloadStr);
+
+  if (strcmp(topic, relayTopic) == 0)
+  { // Compare with the control topic
+    if (payloadStr == "relay1on")
+    {
+      digitalWrite(RY1, HIGH); // Open the first relay
+    }
+    else if (payloadStr == "relay1off")
+    {
+      digitalWrite(RY1, LOW); // Close the first relay
+    }
+    else if (payloadStr == "led1on")
+    {
+      digitalWrite(LED1, LOW); // Turn on the LED
+    }
+    else if (payloadStr == "led1off")
+    {
+      digitalWrite(LED1, HIGH); // Turn off the LED
+    } else if (payloadStr == "ping") {
+      mqtt.publish(relayTopic, "pong");
+    }
+  }
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  pinMode(RY1, OUTPUT);
+  pinMode(LED1, OUTPUT);
+
+  // Connect to WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.setAutoReconnect(true);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Set the MQTT server and port
+  mqtt.setClient(wlan);
+  mqtt.setServer(mqttServer, 1883);
+  mqtt.setKeepAlive(15);
+  mqtt.setCallback(onMessage);
+}
+
+void loop()
+{
+  if (!mqtt.connected())
+  {
+    reconnect();
+  }
+  mqtt.loop();
+}
